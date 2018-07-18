ddfadc7a7cf85eae4d2c9dc6a628814bceebe32d
Ricardo Herrero <herrero.ricardo@gmail.com>
2018-07-17 22:10:44 -0300

    Layout mobile

--- robo.js
+++ robo.js
@@ -13,4 +13,14 @@ function exibirTraducao(consulta){
     setTimeout(iniciarDicionario,2000);
 }
 
+/* iOS re-orientation fix */
+if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i)) {
+    /* iOS hides Safari address bar */
+    window.addEventListener("load",function() {
+        setTimeout(function() {
+            window.scrollTo(0, 1);
+        }, 1000);
+    });
+}
+
 iniciarDicionario();
